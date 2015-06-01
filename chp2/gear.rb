df937597c5052636c54e818321e01786c1c45162
J. Spencer Hopkins <jspencerhopkins@gmail.com>
2015-06-01 16:11:15 -0400

    Wrap up chapter 2

--- chp2/gear.rb
+++ chp2/gear.rb
@@ -1,10 +1,9 @@
 class Gear
-  attr_reader :chainring, :cog, :rim, :tire
-  def initialize(chainring, cog, rim, tire)
+  attr_reader :chainring, :cog, :rim, :wheel
+  def initialize(chainring, cog, wheel=nil)
     @chainring  = chainring
     @cog        = cog
-    @rim        = rim
-    @tire       = tire
+    @wheel      = wheel
   end
 
   def ratio
@@ -13,10 +12,29 @@ class Gear
 
   def gear_inches
     # tire goes around rim twice for diameter
-    ratio * (rim + (tire * 2))
+    ratio * wheel.diameter
   end
-
 end
 
-puts Gear.new(52, 11, 26, 1.5).gear_inches
-puts Gear.new(52, 11, 24, 1.25).gear_inches
+class Wheel
+  attr_reader :rim, :tire
+
+  def initialize(rim, tire)
+    @rim        = rim
+    @tire       = tire
+  end
+
+  def diameter
+    rim + (tire * 2)
+  end
+
+  def circumference
+    diameter * Math::PI
+  end
+end
+
+@wheel = Wheel.new(26, 1.5)
+puts @wheel.circumference
+
+puts Gear.new(52, 11, @wheel).gear_inches
+puts Gear.new(52, 11).ratio
